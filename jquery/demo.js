c19d7e9c96766653fc3f06df626d498099f7bc65
Steve <erock_34@yahoo.com>
2024-06-03 19:33:37 -0500

    added jquery

---
+++ jquery/demo.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("p").css({color:"darkred","margin-top":"20px"});
+    $(".redFont");
+    $(".myTag").css({color:"darkred","margin-top":"20px"});
+
+$("#hide").on("click",function(){
+    $("p").hide();
+});
+$("#show").on("click",function(){
+    $("p").show();
+});
+$("#toggle").on("click",function(){
+    $("h1").toggle();
+
+});
+});
+//$(function(){
+
+//})
