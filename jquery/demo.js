105d3464e2bfcff9d210f86156320133af230a6c
Steve <erock_34@yahoo.com>
2024-06-10 18:41:31 -0500

    last

--- jquery/demo.js
+++ jquery/demo.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("p").css({color:"darkred","margin-top":"20px"});
     $(".redFont");
     $(".myTag").css({color:"darkred","margin-top":"20px"});
-
+    $("[href]").css("color","#777");
 $("#hide").on("click",function(){
     $("p").hide();
 });
